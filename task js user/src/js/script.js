2196dd9dcdc9be278f1e00acf54872d334cfa95e
Mirze26 <mirzaab@code.edu.az>
2024-05-07 13:38:26 +0400

    user register only 18 age

---
+++ task js user/src/js/script.js
@@ -0,0 +1,19 @@
+
+
+let name = prompt("Please write your name");
+let surname = prompt("Please write your surname");
+let age = prompt("Please write your age")
+
+if (age == 18) {
+    alert("Qeydiyyat uqurla tamamlandi");
+}
+else if (age > 18) {
+    alert("Qeydiyyatcun yawivuz coxdu");
+}
+else {
+    alert("Qeydiyyatcun yawivuz azdi");
+}
+
+
+
+
